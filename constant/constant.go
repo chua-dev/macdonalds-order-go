99bebe446a5dabf93e8c6cec13c76e8fb7e31dd8
Chua <chua3940@gmail.com>
2025-02-25 13:26:18 +0800

    Complete

---
+++ constant/constant.go
@@ -0,0 +1,25 @@
+package constant
+
+type RestaurentCommand string
+
+const (
+	NewNormalOrder RestaurentCommand = "New Normal Order"
+	NewVipOrder    RestaurentCommand = "New VIP Order"
+	AddBot         RestaurentCommand = "+ Bot"
+	RemoveBot      RestaurentCommand = "- Bot"
+	Exit           RestaurentCommand = "Exit"
+)
+
+type RestaurentCommandID string
+
+const (
+	NewNormalOrderId RestaurentCommandID = "1"
+	NewVipOrderId    RestaurentCommandID = "2"
+	AddBotId         RestaurentCommandID = "3"
+	RemoveBotId      RestaurentCommandID = "4"
+	ExitId           RestaurentCommandID = "5"
+)
+
+const (
+	DefaultProcessingTime = 10
+)
---
+++ go.mod
@@ -0,0 +1,3 @@
+module macdonald-order-service
+
+go 1.22.0
